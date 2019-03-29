8f911aca152cb1d0758f7c0d6b39a22384615494
luckygamerv2 <45069336+luckygamerv2@users.noreply.github.com>
2019-03-29 16:47:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,9 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const najzx = '#'
+const aprefix ="!";
 
-client.on('ready', () => {
-   console.log(----------------);
-      console.log(By : :"}.);
-        console.log(---------------);
-      console.log(ON ${client.guilds.size} Servers);
-    console.log(---------------);
-  console.log(Logged in as ${client.user.tag}!);
-  client.user.setGame(`);
-}); // 
-
-
-client.on('guildMemberAdd', member  => {
-var channel = member.guild.channels.find('name', 'chat')
-         console.log('-');
-    console.log('-');
-    console.log('-');
-    console.log('-');
-
-
-if (!channel) return;
-   
-
-       
-                          
-                           
-                   setTimeout(() => {
-            
-               channel.send(${member}WELOCME TO Lote SSRVER 
-■■■■اتمني تستمتع معانا■■■■:heart:
-
-`) ;  
-},5000)                            
+client.on("guildMemberAdd", member => {
+  client.channels.find('id', 'ا560421350867730442').send(**. welcome To LOte servre  ${member}اتمني تستمتع معانا **)
 });
 
 client.login(process.env.BOT_TOKEN);
